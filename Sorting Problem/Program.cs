0a058cd341522938dc83cf27b81fa1d3a7415d6d
OludayoDahunsi <122569406+OludayoDahunsi@users.noreply.github.com>
2023-03-04 13:56:29 -0600

    Sort

---
+++ Sorting Problem/Program.cs
@@ -0,0 +1,25 @@
+﻿//Written by Oludayo Dahunsi
+//02/27/2023
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Problem
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> myNumbers = new List<int>() { 5, 2, 7, 1, 8, 3 };
+            Sorter mySorter = new Sorter(myNumbers);
+            Console.WriteLine("Unsorted List: " + string.Join(", ", myNumbers));
+
+            Console.WriteLine("Sorted using Bubble Sort: " + string.Join(", ", mySorter.BubbleSort()));
+            Console.WriteLine("Sorted using Selection Sort: " + string.Join(", ", mySorter.SelectionSort()));
+            Console.WriteLine("Sorted using Insertion Sort: " + string.Join(", ", mySorter.InsertionSort()));
+        }
+    }
+}
---
+++ Sorting Problem/Sorter.cs
@@ -0,0 +1,73 @@
+﻿//Written by Oludayo Dahunsi
+//02/27/2023
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Problem
+{
+    public class Sorter
+    {
+        private List<int> aList;
+
+        public Sorter(List<int> aList)
+        {
+            this.aList = aList;
+        }
+
+        public List<int> BubbleSort()
+        {
+            for (int i = 0; i < aList.Count - 1; i++)
+            {
+                for (int j = 0; j < aList.Count - i - 1; j++)
+                {
+                    if (aList[j] > aList[j + 1])
+                    {
+                        int temp = aList[j];
+                        aList[j] = aList[j + 1];
+                        aList[j + 1] = temp;
+                    }
+                }
+            }
+            return aList;
+        }
+
+        public List<int> SelectionSort()
+        {
+            for (int i = 0; i < aList.Count - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < aList.Count; j++)
+                {
+                    if (aList[j] < aList[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = aList[i];
+                aList[i] = aList[minIndex];
+                aList[minIndex] = temp;
+            }
+            return aList;
+        }
+
+        public List<int> InsertionSort()
+        {
+            for (int i = 1; i < aList.Count; i++)
+            {
+                int key = aList[i];
+                int j = i - 1;
+                while (j >= 0 && aList[j] > key)
+                {
+                    aList[j + 1] = aList[j];
+                    j--;
+                }
+                aList[j + 1] = key;
+            }
+            return aList;
+        }
+    }
+}
